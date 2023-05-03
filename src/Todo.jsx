e879e24f70162b3b7c4c9324dd6a1bfc85baf8eb
kenosama <pc.tcanomorant@me.com>
2023-05-03 14:58:20 +0200

    🐛 fix(Todo.jsx): fix typo in CheckBox import statement ✨ feat(Todo.jsx): change ToDos array to Todos object array with todo property The import statement for CheckBox component had a typo, which has been fixed. The ToDos array has been changed to an array of objects with a text property and a todo property. This change allows for better management of the todo items, as the todo property can be used to keep track of whether an item has been completed or not.
    🎨 style(Todo.jsx): refactor TodoList component to use destructuring and improve readability
    ✨ feat(Todo.jsx): add functionality to handle checking and deleting todos
    Refactored the TodoList component to use destructuring and improve readability. Added functionality to handle checking and deleting todos. The handleChecked function toggles the "done" property of the item at the specified index, while the handleDelete function removes the item at the specified index.

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,17 +1,25 @@
 //importing the necessary dependencies from React
 import { useRef, useState } from "react";
 import Button from "./Components/Shared/Button";
-import ListElement from "./Components/ListElement"
+import ListElement from "./Components/ListElement";
 import Slugify from "./Components/Shared/Slugify";
-import CheckBox from "./Components/Shared/CheckedBox.jsx";
-//Creating the TodoList Component
-const TodoList= () =>{
 
+//Creating the TodoList Component
+const TodoList = () => {
   //creating the array of ToDos, and the variable for the input of the form where the data will be stored.
-  const [ToDos, setToDos] = useState([
-    "learn React",
-    "Master it",
-    "get a Job"
+  const [Todos, setToDos] = useState([
+    {
+      text: "learn React",
+      todo: false,
+    },
+    {
+      text: "Master It",
+      todo: false,
+    },
+    {
+      text: "Get a Job",
+      todo: false,
+    },
   ]);
   const contentRef = useRef();
   //variables for the form
@@ -20,55 +28,78 @@ const TodoList= () =>{
   const label = Slugify(name);
   const placeholder = "Write the Todo";
 
-  const handleSubmitForm = (e)=>{
+  const handleSubmitForm = (e) => {
     e.preventDefault();
-    const postedFormRef = contentRef.current.value
-    console.log(postedFormRef)
-
-    const newToDos = [...ToDos];
-    newToDos.push(postedFormRef);
-    setToDos(newToDos);
+    const postedFormRef = contentRef.current.value;
+    console.log(postedFormRef);
+    const newToDos = {
+      text: postedFormRef,
+      done: false,
+    };
+    setToDos([...Todos, newToDos]);
+    contentRef.current.value = "";
   };
 
-  const deleteToDo = (e)=>{
-    e.preventDefault();
-
+  const handleChecked = (index) => {
+    // Make a copy of the current "todos" array using the spread operator.
+    const newTodos = [...Todos];
+    // Toggle the "done" property of the item at the specified index.
+    newTodos[index].done = !newTodos[index].done;
+    // Update the "todos" state with the new array.
+    setToDos(newTodos);
+  };
+  const handleDelete = (index) => {
+    // Make a copy of the current "todos" array using the spread operator.
+    const newTodos = [...Todos];
+    // Use the splice method to remove the item at the specified index.
+    newTodos.splice(index, 1);
+    // Update the "todos" state with the new array.
+    setToDos(newTodos);
   };
 
-  
-
-
-    return (
-      <div>
-        <div className="container">
-          <form onSubmit={handleSubmitForm}>
-            <label htmlFor={label}>{name}:</label> &nbsp;
-            <input
-              type={type}
-              name={name}
-              id={Slugify(name)}
-              ref={contentRef}
-              placeholder={placeholder}
-            />
-            <Button type="submit" text="Add todo" />
-          </form>
-        </div>
-        <div className="container">
-          <h1>The To do's</h1>
-          <ul>
-            {ToDos.map((todo, index) => (
-              <li key={index}>
-                &nbsp;
-                <input type="checkbox" value="0" name={todo} id={index} onClick={deleteToDo}/>
-                &nbsp;
-                {todo}
-              </li>
-            ))}
-          </ul>
-        </div>
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={handleSubmitForm}>
+          <label htmlFor={label}>{name}:</label> &nbsp;
+          <input
+            type={type}
+            name={name}
+            id={Slugify(name)}
+            ref={contentRef}
+            placeholder={placeholder}
+          />
+          <Button type="submit" text="Add todo" />
+        </form>
       </div>
-    );
-
+      <div className="container">
+        <h1>The To do's</h1>
+        <ul>
+          {Todos.map((name, index) => (
+            <li key={index}>
+              <input
+                type="checkbox"
+                value={name.todo}
+                name={Slugify(name.text)}
+                id={index}
+                onChange={() => handleChecked(index)}
+              />
+              &nbsp;
+              <span
+                style={{
+                  textDecoration: name.done ? "line-through" : "none",
+                }}
+              >
+                {name.text}
+              </span>
+              &nbsp;&nbsp;
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 // Export the TodoList component as the default export of the module.
 export default TodoList;
